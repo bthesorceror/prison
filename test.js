e0ddd7f9b960a3802dd50d118b87b3b9866f92c5
Brandon Farmer and Jon Allured <WkdWMlFHaGhjMmh5YjJOclpYUXVZMjl0+bthesorcer+jonallured@WkdWMlFHaGhjMmh5YjJOclpYUXVZMjl0>
2013-08-12 15:31:49 -0400

    Adds tests around functionality

--- test.js
+++ test.js
@@ -1,30 +1,127 @@
 var tape = require('tape');
 var Prison = require('./index');
 
-(function() {
-  tape('Stores correct cache', function(t) {
-    t.plan(3);
+var default_time = 15 * 60 * 1000;
+var key = 'some_key';
 
-    var prison = new Prison(15 * 60 * 1000);
-    var key = 'some_key';
+(function() {
+
+  tape('Stores correct cache', function(test) {
+    test.plan(3);
+
+    var prison = new Prison(default_time);
 
     var func = function(handler) {
-      t.ok(true, 'tried to access value for cache');
+      test.ok(true, 'tried to access value for cache');
       handler.done("test");
     }
 
-    warden1 = prison.incarcerate(key, func);
+    var warden1 = prison.incarcerate(key, func);
 
     warden1.on('done', function(value) {
-      t.equals(value, "test", "gets correct value");
+      test.equals(value, "test", "gets correct value");
     });
 
     setTimeout(function() {
-      warden2 = prison.incarcerate(key, func);
+      var warden2 = prison.incarcerate(key, func);
 
       warden2.on('done', function(value) {
-        t.equals(value, "test", "gets correct value");
+        test.equals(value, "test", "gets correct value");
       });
     }, 100);
+  });
+
+  (function() {
+    var func = function(handler) {
+      handler.done("test");
+    }
+
+    tape('Uses default time', function(test) {
+      var prison = new Prison(default_time);
+      test.plan(1);
+      var warden1 = prison.incarcerate(key, func);
+
+      prison._set = function(key, result, time) {
+        test.equal(time, default_time);
+      };
+    });
+
+    tape('Uses modified time', function(test) {
+      var prison = new Prison(default_time);
+
+      test.plan(1);
+
+      var func = function(handler) {
+        handler.done("test");
+      }
+
+      var warden1 = prison.incarcerate(key, 9000, func);
+
+      prison._set = function(key, result, time) {
+        test.equal(time, 9000);
+      };
+    });
+
+  })();
+})();
+
+(function() {
+  tape('value returned is from cache', function(test) {
+    test.plan(1);
+
+    var backend = {
+      get: function() {
+        return { result: "1", time: (new Date()).getTime() * 1000000 }
+      }
+    }
+
+    var prison = new Prison(default_time, backend);
+    var warden1 = prison.incarcerate(key, function(handler) {
+      handler.done("2");
+    });
+
+    warden1.on('done', function(value) {
+      test.equal(value, "1");
+    });
+  });
+
+  tape('value returned is from function when key is not found', function(test) {
+    test.plan(1);
+
+    var backend = {
+      get: function() {
+        return null;
+      },
+      set: function() { }
+    }
+
+    var prison = new Prison(default_time, backend);
+    var warden1 = prison.incarcerate(key, function(handler) {
+      handler.done("2");
+    });
+
+    warden1.on('done', function(value) {
+      test.equal(value, "2");
+    });
+  });
+
+  tape('value returned is from function on stale time', function(test) {
+    test.plan(1);
+
+    var backend = {
+      get: function() {
+        return { result: "1", time: 0 }
+      },
+      set: function() { }
+    }
+
+    var prison = new Prison(default_time, backend);
+    var warden1 = prison.incarcerate(key, function(handler) {
+      handler.done("2");
+    });
+
+    warden1.on('done', function(value) {
+      test.equal(value, "2");
+    });
   });
 })();
